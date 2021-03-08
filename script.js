e0ea30e379fbb8d17c37f0a9db4798c1c144da1e
Sean Monaghan <smonagha@conncoll.edu>
2021-03-07 21:46:05 -0500

    added images for the README file

--- script.js
+++ script.js
@@ -14,15 +14,11 @@ function getLyrics(artistName, title) {
         })
         .then(function (data) {
             lyricDiv.empty()
-
-            // console.log(data);
             fixLyrics = data.lyrics.replace('\n', ' ')
             splitLyrics = fixLyrics.split('\n');
 
-
             for (i = 0; i < splitLyrics.length; i++) {
                 let songLine = document.createElement("p");
-
                 songLine.textContent = splitLyrics[i];
                 lyricDiv.append(songLine);
             }
@@ -49,25 +45,10 @@ function getSpotifySong(song) {
         })  
 }
 
-$("#spotifyButton").on('click', function () {
-    console.log("Hello");
-    const songInputValue = searchSong.value;
-    getSpotifySong(songInputValue);
-})
-
-
-function getChords(artistName, songName) {
-    var songsterrURL = "https://www.songsterr.com/a/wa/bestMatchForQueryString?s=" + songName + "&a=" + artistName;
-
-    chordsButton.click(function () {
-        window.open(songsterrURL, "http://127.0.0.1:5500/index.html");
-    });
-}
-
 function createSuggestions(artistName) {
-    var testURL = "https://www.songsterr.com/a/ra/songs.json?pattern=" + artistName;
+    var songsterrURL = "https://www.songsterr.com/a/ra/songs.json?pattern=" + artistName;
 
-    fetch(testURL)
+    fetch(songsterrURL)
         .then(function (response) {
             return response.json();
         })
@@ -98,7 +79,6 @@ searchButton.click(function () {
     const artistInputValue = searchArtist.value;
     const songInputValue = searchSong.value;
     getLyrics(artistInputValue, songInputValue);
-    getChords(artistInputValue, songInputValue);
     createSuggestions(artistInputValue);
 })
 
@@ -108,4 +88,10 @@ chordsButton.click(function () {
     const songName = searchSong.value;
     let chordURL = "https://www.songsterr.com/a/wa/bestMatchForQueryString?s=" + songName + "&a=" + artistName;
     window.open(chordURL, "http://127.0.0.1:5500/index.html");
+})
+
+$("#spotifyButton").on('click', function () {
+    console.log("Hello");
+    const songInputValue = searchSong.value;
+    getSpotifySong(songInputValue);
 })
