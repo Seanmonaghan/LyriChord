cb0acdc8923f68777755ff4aa498c87f1f68af6e
Sean Monaghan <smonagha@conncoll.edu>
2021-03-08 19:50:28 -0500

    updated semantic structure, general code maintanance, added comments

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-var lyricDiv = $("#lyricDiv");
-var searchButton = $("#searchButton");
-var chordsButton = $("#chordsButton");
-var suggestions = $("#suggestions");
-
-// local Storage
+// local Storage Variables
 let savedArtist = localStorage.getItem("savedArtist");
 searchArtist.value = savedArtist;
 let savedSong = localStorage.getItem("savedSong");
 searchSong.value = savedSong;
 
-
+// Function generate lyrics
 function getLyrics(artistName, title) {
     var requestUrl = "https://api.lyrics.ovh/v1/" + artistName + "/" + title;
 
@@ -18,21 +13,21 @@ function getLyrics(artistName, title) {
             return response.json();
         })
         .then(function (data) {
-            lyricDiv.empty()
+            $("#lyricDiv").empty()
             fixLyrics = data.lyrics.replace('\n', ' ')
             splitLyrics = fixLyrics.split('\n');
 
             for (i = 0; i < splitLyrics.length; i++) {
                 let songLine = document.createElement("p");
                 songLine.textContent = splitLyrics[i];
-                lyricDiv.append(songLine);
+                $("#lyricDiv").append(songLine);
             }
         });
 };
 
+// Function to call the Spotify API based on the song input entered
 function getSpotifySong(song) {
     var requestURL = "https://api.spotify.com/v1/search?q=" + song + "&type=track"
-
     fetch(requestURL, {
             headers: {
                 Accept: "application/json",
@@ -44,22 +39,20 @@ function getSpotifySong(song) {
             return response.json();
         })
         .then(function (data) {
-                console.log(data);
-                console.log(data.tracks.items[0].external_urls.spotify);
                 window.open(data.tracks.items[0].external_urls.spotify, "http://127.0.0.1:5500/index.html");
         })  
 }
 
+// Function to create list items filled with songs by the same artist and 
+// makes them link to the songsterr page that song
 function createSuggestions(artistName) {
     var songsterrURL = "https://www.songsterr.com/a/ra/songs.json?pattern=" + artistName;
-
     fetch(songsterrURL)
         .then(function (response) {
             return response.json();
         })
-        .then(function (data) {
-            
-            suggestions.empty()
+        .then(function (data) {      
+            $("#suggestions").empty()
             // console.log(data);
             if (data.length > 5) {
                 suggestionHeader.textContent = "Similar Chords";
@@ -70,36 +63,36 @@ function createSuggestions(artistName) {
                     listItem.href = "http://www.songsterr.com/a/wa/song?id=" + data[i].id;
                     listItem.val = "http://www.songsterr.com/a/wa/song?id=" + data[i].id;
                     listItem.textContent = data[i].artist.name + "  -  " + data[i].title;
-                    suggestions.append(listItem);
+                    $("#suggestions").append(listItem);
                 }
             }
             $(".listItem").on("click", function (e) {
-
                 window.open(this.val, "http://127.0.0.1:5500/index.html");
             })
         })
 }
 
-searchButton.click(function () {
+// Primary Get Lyrics Search Button, also generates suggested songs
+$("#searchButton").click(function () {
     const artistInputValue = searchArtist.value;
     const songInputValue = searchSong.value;
     localStorage.setItem("savedArtist", searchArtist.value);
     localStorage.setItem("savedSong", searchSong.value);
-    
     getLyrics(artistInputValue, songInputValue);
     createSuggestions(artistInputValue);
 })
 
-
-chordsButton.click(function () {
+// Get chords button, links directly to the Songsterr chords page
+$("#chordsButton").click(function () {
     const artistName = searchArtist.value;
     const songName = searchSong.value;
     let chordURL = "https://www.songsterr.com/a/wa/bestMatchForQueryString?s=" + songName + "&a=" + artistName;
     window.open(chordURL, "http://127.0.0.1:5500/index.html");
 })
 
+// Spotify Button, links directly to the Spotify page for the song selected
+// so long as the access key is current
 $("#spotifyButton").on('click', function () {
-    console.log("Hello");
     const songInputValue = searchSong.value;
     getSpotifySong(songInputValue);
 })
